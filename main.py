6c7832bf3d37e8295820b0e994042ad9c99875f5
Luke Turnbull <93167857+fdx-Luke-Turnbull@users.noreply.github.com>
2023-12-09 12:30:20 +1100

    Final add files pre assignment upload

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import logging
 import time
+from location_dict import places
 
 # Set up logging configuration
 logging.basicConfig(level=logging.DEBUG)
@@ -33,6 +34,32 @@ Base.metadata.bind = engine
 
 # Define the Weather class
 class Weather(Base):
+    """
+    ORM class representing the 'weather' table in the database.
+
+    Attributes:
+    - id: Unique identifier for a weather entry.
+    - city: The name of the city.
+    - date: The date of the weather entry.
+    - description: Description of the weather conditions.
+    - temperature: The temperature in the specified unit of measurement.
+    - latitude: The latitude of the city.
+    - longitude: The longitude of the city.
+    - min_temp: The minimum temperature.
+    - max_temp: The maximum temperature.
+    - uom: The unit of measurement for temperature.
+    - feels_like: The perceived temperature.
+    - pressure: Atmospheric pressure.
+    - humidity: Relative humidity.
+    - wind_speed: Wind speed.
+    - wind_deg: Wind direction in degrees.
+    - wind_gust: Wind gust speed.
+    - cloud: Cloud coverage.
+
+    Methods:
+    - __repr__: String representation of the Weather object.
+
+    """
     __tablename__ = 'weather'
     __table_args__ = {'autoload': True}
 
@@ -51,13 +78,11 @@ class Weather(Base):
                 f"pressure={self.pressure},"
                 f"humidity={self.humidity},"
                 f"wind_speed={self.wind_speed},"
-                f"wind_deg={self.wind_deg},"
-                f"wind_gust={self.wind_gust},"
                 f"cloud={self.cloud}>")
 
 
 # Drop the existing 'weather' table to help with bug fixing
-Weather.__table__.drop(engine)
+# Weather.__table__.drop(engine)
 
 # Recreate the 'weather' table
 Base.metadata.create_all(engine)
@@ -68,6 +93,28 @@ if 'weather' not in inspector.get_table_names():
 
 # Create a session
 Session = sessionmaker(bind=engine)
+
+
+# Function to retrieve the API key from the configuration file
+def get_api_key():
+    """
+    Retrieve the OpenWeatherMap API key from the configuration file.
+
+    Returns:
+    str or None: API key if found, else None.
+    """
+    try:
+        # Read the API key from the configuration file
+        configuration = configparser.ConfigParser()
+        configuration.read('config.ini')
+        return configuration['openweathermap']['api']
+
+    except KeyError:
+        logging.error("Error: API key not found in the configuration file.")
+    except FileNotFoundError:
+        logging.error("Error: Configuration file not found.")
+    except configparser.NoSectionError or configparser.NoOptionError:
+        logging.error("Error: Invalid configuration file format.")
 
 
 def make_weather_request(url):
@@ -103,11 +150,21 @@ def get_weather(city_name, uom):
     Parameters:
     - city_name (str): Name of the city.
     - uom (str): Unit of measurement for temperature.
+    - latitude (float): Latitude of the location (optional).
+    - longitude (float): Longitude of the location (optional).
 
     Returns:
     Tuple or None: Tuple containing weather description and temperature if successful, else None.
     """
-    response_dict = make_weather_request(f"data/2.5/weather?q={city_name}&units={uom}")
+    if city_name in places:
+        latitude = places[city_name]['latitude']
+        longitude = places[city_name]['longitude']
+        # Use latitude and longitude to make the weather request
+        response_dict = make_weather_request(f"data/2.5/weather?lat={latitude}&lon={longitude}&units={uom}")
+    else:
+        # Use city_name to make the weather request
+        response_dict = make_weather_request(f"data/2.5/weather?q={city_name}&units={uom}")
+
     if response_dict:
         print("Response Dict:", response_dict)
         try:
@@ -147,14 +204,16 @@ def get_weather(city_name, uom):
             session.commit()
             session.close()
 
-            return description, temperature
+            return (
+                description, temperature, latitude, longitude, max_temp, min_temp,
+                feels_like, pressure, humidity, wind_speed, cloud)
+
         except (KeyError, IndexError):
             # Handle parsing errors
             logging.error(f"[!] Unable to parse weather response for {city_name}: {response_dict}")
             return None
 
 
-# Function to retrieve weather forecast responses for multiple cities
 def get_weather_forecast(city_name, uom, cnt):
     """
     Get weather forecast information for a city from the OpenWeatherMap API.
@@ -163,11 +222,20 @@ def get_weather_forecast(city_name, uom, cnt):
     - city_name (str): Name of the city.
     - uom (str): Unit of measurement for temperature.
     - cnt (int): Number of forecast data points.
+    - latitude (float): Latitude of the location (optional).
+    - longitude (float): Longitude of the location (optional).
 
     Returns:
     Dict or None: Dictionary containing max temperature per day if successful, else None.
     """
-    response_dict = make_weather_request(f"data/2.5/forecast?q={city_name}&units={uom}&cnt={cnt}")
+    if city_name in places:
+        latitude = places[city_name]['latitude']
+        longitude = places[city_name]['longitude']
+        # Use latitude and longitude to make the forecast request
+        response_dict = make_weather_request(f"data/2.5/forecast?lat={latitude}&lon={longitude}&units={uom}&cnt={cnt}")
+    else:
+        # Use city_name to make the forecast request
+        response_dict = make_weather_request(f"data/2.5/forecast?q={city_name}&units={uom}&cnt={cnt}")
 
     if response_dict:
         try:
@@ -181,13 +249,14 @@ def get_weather_forecast(city_name, uom, cnt):
                     max_temp_per_day[fdate] = ftemp
 
             return max_temp_per_day
+
         except (KeyError, IndexError):
             # Handle parsing errors
             logging.error(f"[!] Unable to parse forecast response for {city_name}: {response_dict}")
+
     return None
 
 
-# Function to get weather forecast responses for multiple cities
 def get_weather_forecast_responses(cities, last_city, uom, unit, cnt):
     """
     Get weather forecast responses for multiple cities.
@@ -223,6 +292,134 @@ def get_weather_forecast_responses(cities, last_city, uom, unit, cnt):
     return response
 
 
+def get_units_from_message(user_message):
+    """
+    Extracts temperature unit, unit of measurement (UOM), and speed from the user's message.
+
+    :param user_message: The message provided by the user.
+    :return: A tuple containing temperature unit, unit of measurement, and speed.
+    """
+
+    # Check if the user mentioned "metric" or "imperial" in the message
+    if any(token.text.lower() in ['metric', 'celsius'] for token in nlp(user_message)):
+        # Set unit to Celsius, UOM to metric, and speed to meter/sec
+        unit, uom, speed = 'celsius', 'metric', 'meter/sec'
+    elif any(token.text.lower() in ['imperial', 'fahrenheit'] for token in nlp(user_message)):
+        # Set unit to Fahrenheit, UOM to imperial, and speed to miles/hour
+        unit, uom, speed = 'fahrenheit', 'imperial', 'miles/hour'
+    else:
+        # If no specific unit is mentioned, use the last known values or default to imperial
+        unit, uom, speed = (session.get('last_unit') or 'fahrenheit',
+                            session.get('last_uom') or 'imperial',
+                            session.get('last_speed') or 'miles/hour')
+
+    # Update the session variables with the latest values
+    session['last_unit'], session['last_uom'], session['last_speed'] = unit, uom, speed
+
+    # Return the extracted unit, UOM, and speed
+    return unit, uom, speed
+
+
+def get_weather_response(user_message):
+    """
+    Generates a response about the current weather based on the user's message.
+
+    :param user_message: The message provided by the user.
+    :return: A response about the current weather or None if the message doesn't match the expected format.
+    """
+
+    # Predefined statement to detect if the user is asking about the current weather in a city
+    weather_statement = nlp("Current weather in a city")
+
+    # Tokenize the user's message
+    user_message_doc = nlp(user_message)
+
+    # Retrieve last known values for unit, UOM, and city from the session
+    last_unit, last_uom, last_city = session.get('last_unit'), session.get('last_uom'), session.get('last_city')
+
+    # Check if the user mentioned "metric" or "imperial" in the message and get the unit and UOM
+    unit, uom, speed = get_units_from_message(user_message)
+
+    # Helper function to generate response details based on user's question
+    def generate_response_details(city):
+        weather_result = get_weather(city, uom)
+
+        if weather_result is not None:
+            # Unpack the result only if it's not None
+            (description, temperature, latitude, longitude, max_temp, min_temp,
+             feels_like, pressure, humidity, wind_speed, cloud) = weather_result
+
+            # Generate a dynamic response based on the detected units
+            response_template = f"In {city}, the current weather is {temperature}° {unit} with {description}."
+
+            # Check user's question for specific details
+            if 'wind' in user_message.lower():
+                response_template += f" The wind speed in {city} is {wind_speed} {speed}."
+
+            if 'pressure' in user_message.lower():
+                response_template += f" The pressure in {city} is {pressure} hPa."
+
+            if 'humid' in user_message.lower():
+                response_template += f" The humidity in {city} is {humidity}%."
+
+            if any(keyword in user_message.lower() for keyword in ['longitude', 'latitude', 'map']):
+                response_template += f" {city} is located at coordinates: {latitude}, {longitude}."
+
+            if 'cloud' in user_message.lower():
+                response_template += f" The cloud coverage in {city} is {cloud}%."
+
+            return response_template
+        else:
+            return f"Couldn't retrieve weather information for {city}."
+
+    # Check if the user's message matches the predefined weather statement
+    if weather_statement.similarity(user_message_doc) >= 0.75:
+        # Check for exact matches from location_dict in the user's message
+        location_matches = [location for location in places.keys() if location in user_message]
+
+        if not location_matches:
+            # Extract cities from the user's statement
+            cities = [ent.text for ent in user_message_doc.ents if ent.label_ == "GPE"]
+
+            if not cities:
+                # Use the last city if available, otherwise, ask the user to provide a city
+                city = last_city or None
+                if not city:
+                    return "You need to tell me a city to check."
+        else:
+            # Use the first matching location from location_dict
+            city = location_matches[0]
+            session['last_city'] = city  # Store the last city in the session
+            return generate_response_details(city)
+
+        # Generate responses for multiple cities if mentioned
+        responses = [generate_response_details(city) for city in cities or [last_city]]
+        return "\r\n".join(responses)
+
+    return None
+
+
+def process_weather_forecast_request(user_message):
+    """
+    Processes a user's request for weather forecasts for specific cities.
+
+    :param user_message: The message provided by the user.
+    :return: A response with weather forecasts or an appropriate message if there is an issue.
+    """
+
+    # Extract cities from the user's statement
+    cities = [ent.text for ent in nlp(user_message).ents if ent.label_ == "GPE"]
+
+    # Retrieve the last known city from the session
+    last_city = session.get('last_city')
+
+    # Check if the user mentioned "metric" or "imperial" in the message
+    unit, uom, speed = get_units_from_message(user_message)
+
+    # Get forecast responses with a default count of 56 (can be adjusted)
+    return get_weather_forecast_responses(cities, last_city, uom, unit, cnt=56)
+
+
 # Initialize ChatterBot instance
 my_bot = ChatBot(name="PyBot",
                  read_only=False,
@@ -233,7 +430,8 @@ my_bot = ChatBot(name="PyBot",
                  logic_adapters=[
                      {
                          'import_path': 'chatterbot.logic.BestMatch',
-                         'default_response': 'Sorry, I am not sure how to respond.'
+                         'default_response':
+                             'Sorry, I am not sure how to respond. Try asking me the weather in a nearby city.'
                      },
                  ],
                  database_uri='sqlite:///db.sqlite3',
@@ -242,7 +440,7 @@ my_bot = ChatBot(name="PyBot",
                  ]
                  )
 
-# Define small talk conversations for training
+# Train ChatterBot
 small_talk = [
     "Hello",
     "Hi there!",
@@ -258,9 +456,13 @@ list_trainer = ListTrainer(my_bot)
 for item in small_talk:
     list_trainer.train(item)
 
+travel_talk = open('static/training_data/travel_talk.txt').read().splitlines()
+# training_data_weather = open('static/training_data/weather_questions.txt').read().splitlines()
+list_trainer.train(travel_talk)
+
 # Train ChatterBot with English language corpus
 corpus_trainer = ChatterBotCorpusTrainer(my_bot)
-corpus_trainer.train('chatterbot.corpus.english.humor',
+corpus_trainer.train(
                      "chatterbot.corpus.english.greetings",
                      "chatterbot.corpus.english.conversations"
                      )
@@ -326,132 +528,6 @@ def get_bot_response():
         # If the weather_response is None, use the chatbot to generate a response
         # Otherwise, return the weather_response
         return weather_response or str(my_bot.get_response(user_message))
-
-
-def process_weather_forecast_request(user_message):
-    """
-    Processes a user's request for weather forecasts for specific cities.
-
-    :param user_message: The message provided by the user.
-    :return: A response with weather forecasts or an appropriate message if there is an issue.
-    """
-
-    # Extract cities from the user's statement
-    cities = [ent.text for ent in nlp(user_message).ents if ent.label_ == "GPE"]
-
-    # Retrieve the last known city from the session
-    last_city = session.get('last_city')
-
-    # Check if the user mentioned "metric" or "imperial" in the message
-    unit, uom, speed = get_units_from_message(user_message)
-
-    # Get forecast responses with a default count of 56 (can be adjusted)
-    return get_weather_forecast_responses(cities, last_city, uom, unit, cnt=56)
-
-
-def get_units_from_message(user_message):
-    """
-    Extracts temperature unit, unit of measurement (UOM), and speed from the user's message.
-
-    :param user_message: The message provided by the user.
-    :return: A tuple containing temperature unit, unit of measurement, and speed.
-    """
-
-    # Check if the user mentioned "metric" or "imperial" in the message
-    if any(token.text.lower() in ['metric', 'celsius'] for token in nlp(user_message)):
-        # Set unit to Celsius, UOM to metric, and speed to meter/sec
-        unit, uom, speed = 'celsius', 'metric', 'meter/sec'
-    elif any(token.text.lower() in ['imperial', 'fahrenheit'] for token in nlp(user_message)):
-        # Set unit to Fahrenheit, UOM to imperial, and speed to miles/hour
-        unit, uom, speed = 'fahrenheit', 'imperial', 'miles/hour'
-    else:
-        # If no specific unit is mentioned, use the last known values or default to imperial
-        unit, uom, speed = (session.get('last_unit') or 'fahrenheit',
-                            session.get('last_uom') or 'imperial',
-                            session.get('last_speed') or 'miles/hour')
-
-    # Update the session variables with the latest values
-    session['last_unit'], session['last_uom'], session['last_speed'] = unit, uom, speed
-
-    # Return the extracted unit, UOM, and speed
-    return unit, uom, speed
-
-
-def get_weather_response(user_message):
-    """
-    Generates a response about the current weather based on the user's message.
-
-    :param user_message: The message provided by the user.
-    :return: A response about the current weather or None if the message doesn't match the expected format.
-    """
-
-    # Predefined statement to detect if the user is asking about the current weather in a city
-    weather_statement = nlp("Current weather in a city")
-
-    # Tokenize the user's message
-    user_message_doc = nlp(user_message)
-
-    # Retrieve last known values for unit, UOM, and city from the session
-    last_unit, last_uom, last_city = session.get('last_unit'), session.get('last_uom'), session.get('last_city')
-
-    # Check if the user mentioned "metric" or "imperial" in the message and get the unit and UOM
-    unit, uom = get_units_from_message(user_message)[:2]
-
-    # Check if the user's message matches the predefined weather statement
-    if weather_statement.similarity(user_message_doc) >= 0.75:
-        # Extract cities from the user's statement
-        cities = [ent.text for ent in user_message_doc.ents if ent.label_ == "GPE"]
-
-        if not cities:
-            # Use the last city if available, otherwise, ask the user to provide a city
-            city = last_city or None
-            if not city:
-                return "You need to tell me a city to check."
-
-        responses = []
-        for city in cities or [last_city]:
-            session['last_city'] = city  # Store the last city in the session
-            weather_result = get_weather(city, uom)
-
-            if weather_result is not None:
-                # Unpack the result only if it's not None
-                description, temperature = weather_result
-
-                # Generate a dynamic response based on the detected units
-                response_template = "In {city}, the current weather is {temperature}° {unit} with {description}."
-                response = response_template.format(
-                    city=city,
-                    unit=unit,
-                    temperature=temperature,
-                    description=description)
-                responses.append(response)
-            else:
-                responses.append(f"Couldn't retrieve weather information for {city}.")
-
-        return "\r\n".join(responses)
-    return None
-
-
-# Function to retrieve the API key from the configuration file
-def get_api_key():
-    """
-    Retrieve the OpenWeatherMap API key from the configuration file.
-
-    Returns:
-    str or None: API key if found, else None.
-    """
-    try:
-        # Read the API key from the configuration file
-        configuration = configparser.ConfigParser()
-        configuration.read('config.ini')
-        return configuration['openweathermap']['api']
-
-    except KeyError:
-        logging.error("Error: API key not found in the configuration file.")
-    except FileNotFoundError:
-        logging.error("Error: Configuration file not found.")
-    except configparser.NoSectionError or configparser.NoOptionError:
-        logging.error("Error: Invalid configuration file format.")
 
 
 # Run the Flask app
--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,53 @@
-       document.getElementById('chat-form').addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevent the default form submission
-            sendMessage();
-            });
+document.getElementById('chat-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    sendMessage();
+    });
 
-        document.getElementById('user-input').addEventListener('keydown', function(event) {
-            // Check if the pressed key is Enter
-            if (event.key === 'Enter') {
-                event.preventDefault(); // Prevent the default behavior (e.g., line break in the input)
-                sendMessage();
-                }
-            });
-
-        async function sendMessage() {
-            try {
-                var userMessage = document.getElementById('user-input').value;
-                document.getElementById('user-input').value = '';
-
-                // Send user message to the server for a response
-                const response = await fetch('/get_response', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: 'user_message=' + encodeURIComponent(userMessage),
-                });
-
-                const data = await response.text();
-
-                // Display user message and chatbot response in the chat display
-                var userMessageElement = '<div class ="user-message-container"><div style="padding:5px;">Me</div><div class="chat-message user-message">' + userMessage + '</div></div>';
-                var chatbotMessageElement = '<div class ="chatbot-message-container"><div style="padding:5px;">WeatherBot</div><div class="chat-message chatbot-message">' + data + '</div></div>';
-                document.getElementById('chat-display').innerHTML += userMessageElement + chatbotMessageElement;
-
-                // Scroll the chat display to the bottom
-                scrollChatToBottom();
-
-            } catch (error) {
-                console.error('Error:', error);
-            }
+document.getElementById('user-input').addEventListener('keydown', function(event) {
+    // Check if the pressed key is Enter
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent the default behavior (e.g., line break in the input)
+        sendMessage();
         }
+    });
 
-        function scrollChatToBottom() {
-            var chatDisplay = document.getElementById('chat-display');
-            chatDisplay.scrollTop = chatDisplay.scrollHeight;
-        }
+async function sendMessage() {
+    try {
+        var userMessage = document.getElementById('user-input').value;
+        document.getElementById('user-input').value = '';
+
+        // Send user message to the server for a response
+        const response = await fetch('/get_response', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'user_message=' + encodeURIComponent(userMessage),
+        });
+
+        const data = await response.text();
+
+        // Display user message immediately
+        var userMessageElement = '<div class ="user-message-container"><div style="padding:5px;">Me</div><div class="chat-message user-message">' + userMessage + '</div></div>';
+        document.getElementById('chat-display').innerHTML += userMessageElement;
+
+        // Introduce a delay before displaying the chatbot's response
+        setTimeout(function () {
+            // Display chatbot response in the chat display
+            var chatbotMessageElement = '<div class ="chatbot-message-container"><div style="padding:5px;">WeatherBot</div><div class="chat-message chatbot-message">' + data + '</div></div>';
+            document.getElementById('chat-display').innerHTML += chatbotMessageElement;
+
+            // Scroll the chat display to the bottom
+            scrollChatToBottom();
+        }, 500); // Adjust the delay duration (in milliseconds) as needed
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+function scrollChatToBottom() {
+    var chatDisplay = document.getElementById('chat-display');
+    chatDisplay.scrollTop = chatDisplay.scrollHeight;
+}
